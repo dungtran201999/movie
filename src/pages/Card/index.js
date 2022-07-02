0697ee218bdcd5837e1e23e5f5e00da796b8f8f3
dung <tranquangdung2099@gmail.com>
2022-07-02 11:18:03 +0700

    up

--- src/pages/Card/index.js
+++ src/pages/Card/index.js
@@ -6,12 +6,12 @@ const Card = ({infoMovie}) => {
         document.body.style.backgroundSize = "cover"
       }, [infoMovie])
     return (
-        <div className="bg-[#000000d9] rounded flex">
-            <div className="w-2/5"><img src={`https://image.tmdb.org/t/p/w500${infoMovie?.poster_path}`} alt="" /></div>
-            <div className="w-3/5 p-6">
-                <h1 className="text-[40px] font-bold">{infoMovie?.original_title}</h1>
+        <div className="bg-[#000000d9] rounded inline-block mb-8 md:mb-0 md:flex">
+            <div className="w-full md:w-2/5"><img src={`https://image.tmdb.org/t/p/w500${infoMovie?.poster_path}`} alt="" /></div>
+            <div className="w-full md:w-3/5 p-4">
+                <h1 className="text-[40px] font-bold ">{infoMovie?.original_title}</h1>
                 <span className="font-[Oswald,sans-serif] text-[#00FC87] text-xl mb-2 block">{infoMovie?.tagline}</span>
-                <p className="text-base mb-4">{infoMovie?.overview}</p>
+                <p className="text-base mb-4 ">{infoMovie?.overview}</p>
                 <div className="mt-9">
                     <span className="font-[Oswald,sans-serif] text-[#00FC87] text-xl">{infoMovie?.genres.map((items,key)=>items.name +', ')}</span>
                     <p className="font-[Oswald,sans-serif] text-base mb-5">{infoMovie?.production_companies.map((items,key)=>items.name +', ')} </p>
--- src/pages/SearchMovie/index.js
+++ src/pages/SearchMovie/index.js
@@ -36,13 +36,13 @@ const SearchMovie=({submitIdMovie})=>{
             <div className="text-right mb-10 mt-5">
                 <input 
                     type="text" 
-                    className="border-b border-[#fff] py-2 px-3 w-[58%] bg-transparent text-white outline-none" 
+                    className="border-b border-[#fff] py-2 px-3 w-full md:w-[58%] bg-transparent text-white outline-none" 
                     placeholder="Search Movie Title..." 
                     value={searchValue}
                     onChange={handleSearchOnChange}
                     />
             </div>
-            <div className={`${showDropdownMenu ? 'translate-y-0 visible opacity-100 transition-all duration-[1000ms]': 'translate-y-5 invisible opacity-0'}  absolute w-[58%] top-full right-0 bg-[#000000c2] pt-1`}>
+            <div className={`${showDropdownMenu ? 'translate-y-0 visible opacity-100 transition-all duration-[1000ms]': 'translate-y-5 invisible opacity-0'}  absolute w-full md:w-[58%] top-full right-0 bg-[#000000c2] pt-1`}>
                 {dataSearch?.map((items,index)=>(
                     <p key={index} onClick={()=>handleOnClickIdMovie({id:items.id,title:items.title})} className="text-[#c0c6d0] mb-1 hover:bg-[#00FC87] py-1 px-3 hover:text-[#000] text-[17px] cursor-pointer">{items.title}</p>
                 ))}
